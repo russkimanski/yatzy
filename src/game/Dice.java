96b434485effd5a4df1d0c15acadd783b1ea3240
russkimanski <50149757+russkimanski@users.noreply.github.com>
2019-11-28 11:40:23 +0100

    update fxml

--- src/game/Dice.java
+++ src/game/Dice.java
@@ -1,24 +1,24 @@
 package game;
 
+/* Zufallszahlen werden an der Stelle i gespeichert.
+ * Diese Zufallszahlen sind dann allerdings Kommazahlen zwischen 0 und 1.
+ * Wenn der Rückgabewert der Methode Math.Random() mit sechs multiplizierst wird, gibt es die Würfelzahlen.
+ * Dieser Wert wird um eins erhöht um die NULL auszuschliessen.
+ * Der Kommawert muss für die Würfelzahlen in einen Integerwert umgewandlet werden - (int).
+ */
+
 public class Dice {
     private String result = "";
 
     public Dice(int numberOfThrows) {
         int[] rollDice = new int[numberOfThrows]; //Die grösse des Arrays wird mit dem Übergabewert anzahlWuerfe bestimmt.
-        for (int i = 0; i < rollDice.length; i++) { //Schleife zum Füllen des Arrays.
+        for (int i = 0; i < rollDice.length; i++) {
             rollDice[i] = (int) (Math.random() * 6) + 1;
-            /* Zufallszahlen werden an der Stelle i gespeichert.
-             * Diese Zufallszahlen sind dann allerdings Kommazahlen zwischen 0 und 1.
-             * Wenn der Rückgabewert der Methode Math.Random() mit sechs multiplizierst wird, gibt es die Würfelzahlen.
-             * Dieser Wert wird um eins erhöht um die NULL auszuschliessen.
-             * Der Kommawert muss für die Würfelzahlen in einen Integerwert umgewandlet werden - (int).
-             */
         }
 
-        //Schleife zum Lesen des Arrays:
         for (int i = 0; i < rollDice.length; i++) {
             if (i + 1 < rollDice.length) {
-                result += rollDice[i] + ", "; //Ausgabe des Wertes im Fach i
+                result += rollDice[i] + ", ";
             } else {
                 result += rollDice[i] + "";
             }
@@ -28,10 +28,10 @@ public class Dice {
 
     public String getResult() {
         return result;
-    } //Methode zur Ausgabe als String
+    }
 
     public void printOut() {
         System.out.println(result);
-    } //Methode zur Ausgabe in Konsole
+    }
 }
 
